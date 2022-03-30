37be0e58ea2a9e96c1b2233bb3c7990688961e6d
EF Devs <emiratesfoundation01@gmail.com>
2022-03-30 18:46:45 +0400

    Create MobilePopup.js

---
+++ MobilePopup.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+	var instance = $.fn.deviceDetector;
+	var IsMobile = instance.getInfo().mobile;
+	if (IsMobile) {
+		if(instance.getInfo().android){
+			$("#iosLink").hide();
+		}else{
+			$("#androidLink").hide();
+		}
+	}
+});
